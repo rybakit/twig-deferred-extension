8d381b1a27f577c9563b9ce0521d864588b467ab
Eugene Leonovich <eugene.leonovich@helloprint.com>
2017-01-12 10:17:32 +0100

    Fix deprecation warnings

--- src/DeferredBlockNode.php
+++ src/DeferredBlockNode.php
@@ -11,7 +11,7 @@ class DeferredBlockNode extends \Twig_Node_Block
         $compiler
             ->write("public function block_$name(\$context, array \$blocks = array())\n", "{\n")
             ->indent()
-            ->write("\$this->env->getExtension('deferred')->defer(\$this, '$name');\n")
+            ->write("\$this->env->getExtension('Phive\\Twig\\Extensions\\Deferred\\DeferredExtension')->defer(\$this, '$name');\n")
             ->outdent()
             ->write("}\n\n")
         ;
@@ -21,7 +21,7 @@ class DeferredBlockNode extends \Twig_Node_Block
             ->write("public function block_{$name}_deferred(\$context, array \$blocks = array())\n", "{\n")
             ->indent()
             ->subcompile($this->getNode('body'))
-            ->write("\$this->env->getExtension('deferred')->resolve(\$this, \$context, \$blocks);\n")
+            ->write("\$this->env->getExtension('Phive\\Twig\\Extensions\\Deferred\\DeferredExtension')->resolve(\$this, \$context, \$blocks);\n")
             ->outdent()
             ->write("}\n\n")
         ;
